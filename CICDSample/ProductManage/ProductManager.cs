b55be777384de6ef17fbaba8adf092f538c61430
JuanPablo <jpgarciaazure@hotmail.com>
2018-04-03 19:41:49 -0400

    First deployment

---
+++ CICDSample/ProductManage/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManage
+{
+    public class ProductManager
+    {
+        public static string GetProducManagerVersion()
+        {
+            return "1.0.0";
+        }
+    }
+}
---
+++ CICDSample/ProductsAPI/ProductManage.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace ProductsAPI
+{
+    public static class ProductManage
+    {
+        [FunctionName("GetVersion")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get",  Route = "products/version")]HttpRequestMessage req, TraceWriter log)
+        {
+          
+            return req.CreateResponse(HttpStatusCode.OK, new { version =1.0}, JsonMediaTypeFormatter.DefaultMediaType);
+        }
+    }
+}
